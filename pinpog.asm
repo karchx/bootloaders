08a6595c6564b77a248d9c50bd084c6448e847cd
KenethSandoval <kenethra.74@gmail.com>
2022-03-05 19:01:57 -0600

    feat: macros colora and size in pinpog

---
+++ pinpog.asm
@@ -0,0 +1,33 @@
+	org 0x7C00
+%define WIDTH   320
+%define HEIGHT  200
+%define COLUMNS 40
+%define ROWS    25
+
+%define COLOR_BLACK  				0
+%define COLOR_BLUE   				1
+%define COLOR_GREEN  				2
+%define COLOR_CYAN   				3
+%define COLOR_RED      			4
+%define COLOR_MAGENTA   		5
+%define COLOR_BROWN  				6
+%define COLOR_LIGHTGRAY  		7
+%define COLOR_DARKGRAY  		8
+%define COLOR_LIGHTBLUE  		9
+%define COLOR_LIGHTGREEN 		10
+%define COLOR_LIGHTCYAN  		11
+%define COLOR_LIGHTRED 		  12
+%define COLOR_LIGHTMAGENTA 	13
+%define COLOR_YELLOW 		  	14
+%define COLOR_WHITE 		  	15
+
+%define BACKGROUND_COLOR COLOR_BLACK
+
+%define BALL_WIDTH  	16
+%define BALL_HEIGHT 	16
+%define BALL_VELOCITY 16
+%define BAR_VELOCITY  10
+
+%define VGA_OFFSET 0xA00 	; direccion del vga
+
+%define SCORE_DIGIT_COUNT 5
